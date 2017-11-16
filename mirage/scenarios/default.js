cf17c144730603549589f07646d9349d88d53cbc
carlogilmar <carlogilmar12@gmail.com>
2017-11-15 22:12:54 -0600

    Adding songs for my default scenario because they are more cool than fixtures

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,3 +1,11 @@
 export default function( server ) {
-  server.createList('book',10);
+  //server.createList('book',10);
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
+  server.create('book',{ title:'La sombra', author:'carlogilamr', year:1998 })
 }
