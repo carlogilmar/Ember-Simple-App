0c848a8ad4c43b93a71bf74f8f142426a75372ee
carlogilmar <carlogilmar12@gmail.com>
2017-11-15 22:05:04 -0600

    Fix error in fixture book

--- mirage/fixtures/book.js
+++ mirage/fixtures/book.js
@@ -1,9 +1,8 @@
 import Mirage, {faker}  from 'ember-cli-mirage';
 
-export default [
+export default Mirage.Factory.extend({
 
-  title: faker.lorem.sentence,
-  author(){ return faker.name.findName() },
-  year: faker.date.post
-
-];
+    title: faker.lorem.sentence,  // using faker
+    author() {return faker.name.findName(); },
+    year: faker.date.past
+});
